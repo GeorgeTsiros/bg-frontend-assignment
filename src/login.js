8513c9147220ac20b98d0bf12d5b5a877bc58415
Giorgos Tsiros <geotsiros@hotmail.com>
2019-10-22 17:24:30 +0300

    login form and regex for ID

--- src/login.js
+++ src/login.js
@@ -1,9 +1,19 @@
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
+
+const idPattern = /[a-zA-Z0-9]{2,8}@[0-9]{4}\.[a-z]{2}/;
+
+
+const validateID = function(input) {
+  return idPattern.test(input);
+};
+
+
+
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
+if ( username == "Formget" && validateID(password)){
 alert ("Login successfully");
 window.location = "success.html"; // Redirecting to other page.
 return false;
@@ -21,8 +31,7 @@ return false;
 }
 }
 
-^([a-zA-Z]{1}-[0-9]{2}-[0-9]{2}[a-zA-Z]{0,1})$
 
 
 //	regex for ID
-/[a-zA-Z0-9]{2,8}@[0-9]{4}\.[a-z]{2}/
+//   /[a-zA-Z0-9]{2,8}@[0-9]{4}\.[a-z]{2}/
