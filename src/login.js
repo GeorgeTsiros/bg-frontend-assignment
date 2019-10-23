44900de0d04d14293cd2c36842718c89ca7dfce7
Giorgos Tsiros <geotsiros@hotmail.com>
2019-10-23 13:37:59 +0300

    mock units page

--- src/login.js
+++ src/login.js
@@ -13,23 +13,23 @@ const validateID = function(input) {
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && validateID(password)){
+if ( validateID(username) && validateID(password)){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "units.html"; // Redirecting to other page.
 return false;
 }
 else{
-attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
+// attempt --;// Decrementing by one.
+alert("Colonist ID is invalid");
 // Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
+// if( attempt == 0){
+// document.getElementById("username").disabled = true;
+// document.getElementById("password").disabled = true;
+// document.getElementById("submit").disabled = true;
+// return false;
 }
 }
-}
+// }
 
 
 
